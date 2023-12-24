8af8f864418ef10d351d31b353cbebc7cc31f65b
bailey barton <baileybarton77@gmail.com>
2023-12-24 16:17:03 -0600

    Resetting Code base due to aged code

--- NBOBanking/src/app/models/BankAccount.ts
+++
@@ -1,7 +0,0 @@
-export interface BankAccount {
-  bankaccount_id: number;
-  user_id: number;
-  account_num: number;
-  total_amount: number;
-  account_type: string;
-}
--- NBOBanking/src/app/models/Transaction.ts
+++
@@ -1,7 +0,0 @@
-export interface Transaction {
-  transaction_id: number;
-  bankaccount_id: number;
-  transaction_type: string;
-  transaction_amount: number;
-  transaction_date: Date;
-}
